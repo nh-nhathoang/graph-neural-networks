fix reduce lr using valid_loss and data leakage before data split

final_model_local.py
@@ -1,17 +1,13 @@
 import argparse
 import os
-from glob import glob
 
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
-from torch.nn import Linear
 from torch_geometric.data import Data
-from torch_geometric.nn import TransformerConv, global_mean_pool
 
 from utils.data_processing import *
 from utils.architecture import GIN, Transformer
@@ -209,10 +205,20 @@ def main():
     all_undirected_after = all(is_undirected(data) for data in dataset)
     print(f"Graphs are undirected (after conversion): {all_undirected_after}")
 
-    dataset = normalize_planar_info(dataset)
-    train_loader, valid_loader, test_loader = prepare_dataset(
-        dataset, batch_size, train_percentage=0.80, valid_percentage=0.1
-    )
+    dataset_size = len(dataset)
+    train_size = int(0.80 * dataset_size)
+    valid_size = int(0.10 * dataset_size)
+    test_size = dataset_size - train_size - valid_size
+    
+    train_set, valid_set, test_set = random_split(dataset, [train_size, valid_size, test_size])
+    
+    # Extract lists to normalize safely using Training criteria
+    train_list, valid_list, test_list = list(train_set), list(valid_set), list(test_set)
+    train_list, valid_list, test_list = normalize_planar_info(train_list, valid_list, test_list)
+
+    train_loader = DataLoader(train_list, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_list, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_list, batch_size=batch_size, shuffle=False)
 
     plot_dataset(dataset, args.save_model_dir)
 

utils/architecture.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.nn import Linear, Sequential, BatchNorm1d, ReLU, Dropout
-from torch_geometric.nn import GINConv, CGConv, GATv2Conv, TransformerConv, NNConv
+from torch_geometric.nn import GINConv, TransformerConv
 from torch_geometric.nn import global_add_pool, global_mean_pool
 
 class GIN(torch.nn.Module):

utils/data_processing.py
@@ -1,9 +1,7 @@
 import torch
 from torch_geometric.loader import DataLoader
-from torch.utils.data import Dataset, random_split
-from itertools import permutations
+from torch.utils.data import random_split
 import networkx as nx
-from torch_geometric.data import Data
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
@@ -85,18 +83,28 @@ def feature_engineering(data_list):
 
     return engineered_data_list
 
-def normalize_planar_info(data_list):
-    for data in data_list:
-        num_features = data.x.shape[1]
-        for i in range(0, num_features):
+def normalize_planar_info(training_list, validation_list = None, test_list = None):
+    all_train_x = torch.cat([data.x for data in training_list], dim=0)
+    num_features = all_train_x.shape[1]
+    
+    global_mins = torch.min(all_train_x, dim=0)[0]
+    global_maxs = torch.max(all_train_x, dim=0)[0]
 
-            min_val = torch.min(data.x[:, i]).item()
-            max_val = torch.max(data.x[:, i]).item()
-            
-            if max_val - min_val != 0:
-                data.x[:, i] = (data.x[:, i] - min_val) / (max_val - min_val)
+    def scale_split(data_list):
+        for data in data_list:
+            for i in range(num_features):
+                denom = global_maxs[i] - global_mins[i]
+                if denom != 0:
+                    data.x[:, i] = (data.x[:, i] - global_mins[i]) / denom
+        return data_list
+    
+    training_list = scale_split(training_list)
+    if validation_list is not None:
+        validation_list = scale_split(validation_list)
+    if test_list is not None:
+        test_list = scale_split(test_list)
 
-    return data_list
+    return training_list, validation_list, test_list
 
 
 def normalize_E(data_list):

utils/train_model.py
@@ -44,11 +44,8 @@ def train_model(model, train_loader, valid_loader, criterion, optimizer, schedul
         R2_train = r2_score(E_whole, out_whole)  
         R2_trainings.append(R2_train)  
         
-        train_losses.append(train_loss / total_graphs)
         epoch_loss = train_loss / total_graphs
-        
-        scheduler.step(torch.tensor(epoch_loss).float())
-        current_lr = scheduler.get_last_lr()[0]
+        train_losses.append(epoch_loss)
 
         model.eval()
         with torch.no_grad():
@@ -76,6 +73,9 @@ def train_model(model, train_loader, valid_loader, criterion, optimizer, schedul
             valid_loss = valid_loss / total_graphs
             valid_losses.append(valid_loss)  
         
+        scheduler.step(valid_loss)  # Step the scheduler based on validation loss
+        current_lr = scheduler.get_last_lr()[0]
+
         if valid_loss < best_loss:
             best_loss = valid_loss
             best_state_dict = copy.deepcopy(model.state_dict())